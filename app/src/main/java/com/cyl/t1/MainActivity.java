2c4fc00ad4e723e35013967c46940761f35b4382
caoyanglong <670654904@qq.com>
2016-07-22 22:52:53 +0800

    修改测试

--- app/src/main/java/com/cyl/t1/MainActivity.java
+++ app/src/main/java/com/cyl/t1/MainActivity.java
@@ -1,8 +1,8 @@
 package com.cyl.t1;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Context context = getApplicationContext();
+
+
+
+
+
     }
 }
